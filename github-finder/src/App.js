b2560e17bf1c815f479aebb2f687121235516dbd
jhetuts <jhetuts@gmail.com>
2019-08-03 03:58:44 +0800

    Added alert catch

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 import axios from 'axios';
 
@@ -10,38 +11,42 @@ class App extends Component {
 	state = {
 		users: [],
 		loading: false,
+		alert: null,
 	};
 
-	// async componentDidMount() {
-	// 	this.setState({ loading: true });
-
-	// 	const res = await axios.get(
-	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
-	// 			process.env.REACT_APP_GITHUB_CLIENT_SECRET
-	// 		}`
-	// 	);
-
-	// 	this.setState({ users: res.data, loading: false });
-	// }
-
 	searchUsers = async text => {
-		this.setState({ loading: true });
+		if (text !== '') {
+			this.setState({ loading: true });
+			const res = await axios.get(
+				`https://api.github.com/search/users?q=${text}&client_id=${
+					process.env.REACT_APP_GITHUB_CLIENT_ID
+				}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			);
+			this.setState({ users: res.data.items, loading: false });
+		}
+	};
 
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${
-				process.env.REACT_APP_GITHUB_CLIENT_ID
-			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		this.setState({ users: res.data.items, loading: false });
+	clearUsers = () => this.setState({ users: [], loading: false });
+
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => this.setState({ alert: null }), 2500);
 	};
 
 	render() {
+		const { users, loading, alert } = this.state;
 		return (
 			<div className='App'>
 				<Navbar icon='fab fa-github' title='Github Finder' />
 				<div className='container'>
-					<Search searchUsers={this.searchUsers} />
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Alert alert={alert} />
+					<Search
+						clearUsers={this.clearUsers}
+						searchUsers={this.searchUsers}
+						setAlert={this.setAlert}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={users} />
 				</div>
 			</div>
 		);
--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -8,13 +8,20 @@ class Search extends Component {
 
 	static propTypes = {
 		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired,
 	};
 
 	onChange = e => this.setState({ [e.target.name]: e.target.value });
 	onSubmit = e => {
 		e.preventDefault();
-		this.props.searchUsers(this.state.text);
-		this.setState({ text: '' });
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter user name', 'danger');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
 	};
 
 	render() {
@@ -30,6 +37,11 @@ class Search extends Component {
 					/>
 					<input type='submit' value='Search' className='btn btn-dark btn-block' />
 				</form>
+				{this.props.showClear && (
+					<button className='btn btn-light btn-block' onClick={this.props.clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
