0cdafb3993c42ec5e88768c9529a85fc314564c8
jhetuts <jhetuts@gmail.com>
2019-08-03 01:04:46 +0800

    Added layout components and dynamic navbar

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Hello There!</h1>
+			<div className='App'>
+				<Navbar icon='fab fa-github' title='Github Finder' />
 			</div>
 		);
 	}
